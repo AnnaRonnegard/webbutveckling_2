0401cf00ec7e55f58920bf5b2c5f35b82babfae3
AnnaRonnegard <a.ronnegard@gmail.com>
2025-05-16 12:23:57 +0200

    camelcase

--- Inlamning_M5_Lek/Javascript/init.js
+++ Inlamning_M5_Lek/Javascript/init.js
@@ -1,4 +1,4 @@
-  function Init() {
+  function init() {
     localStorage.setItem("raknaForlost", 0); //Sparar undan 0 i localStorage "raknaForlost".
     localStorage.setItem("raknaVinst", 0); //Sparar undan 0 i localStorage "raknaVinst".
   }
--- Inlamning_M5_Lek/Javascript/script.js
+++ Inlamning_M5_Lek/Javascript/script.js
@@ -1,4 +1,4 @@
-function Spela() {  //Huvudfunktion. De funktioner som används i den är definierade nedanför.
+function spela() {  //Huvudfunktion. De funktioner som används i den är definierade nedanför.
     
     const urlParams = new URLSearchParams(window.location.search); //Läser in webbläsar-strängen.
     const mittVal = urlParams.get("val"); //Läser in val-värdet i variabeln mittVal.
@@ -6,14 +6,14 @@ function Spela() {  //Huvudfunktion. De funktioner som används i den är defini
     let raknaForlost = parseInt(localStorage.getItem("raknaForlost")); //Läser värdet i localStorage "raknaForlost" och parsar det till variabeln "raknaForlost".
     let raknaVinst = parseInt(localStorage.getItem("raknaVinst")); //Läser värdet i localStorage "raknaVinst" och parsar det till variabeln "raknaVinst".
    
-    let datorVal = SlumpVal();  //Skapar datorvalet.
+    let datorVal = slumpVal();  //Skapar datorvalet.
 
     if (mittVal == datorVal) {  //Behandlar utfallet att bägge val är lika.
       document.getElementById("grattisSorryStorId").innerHTML="Det blev oavgjort";
       document.getElementById("utskriftHalvStorId").innerHTML="Datorn valde också " + mittVal + ".";
       document.getElementById(mittVal+"Id").style.display="block";
       
-      VinstForlustUtskrift(raknaForlost, raknaVinst);  //Se funktionen nedan.
+      vinstForlustUtskrift(raknaForlost, raknaVinst);  //Se funktionen nedan.
       return;  //Avslutar isåfall funktionen.
     }
 
@@ -21,56 +21,56 @@ function Spela() {  //Huvudfunktion. De funktioner som används i den är defini
     switch (mittVal) { 
       case "sten":
         if (datorVal== "påse") {
-          Sorry(datorVal, mittVal);
+          sorry(datorVal, mittVal);
           document.getElementById("utskriftId").innerHTML="Stenen omslutes av påsen.";
           document.getElementById("paseStenId").style.display="block";
           raknaForlost ++;
           localStorage.setItem("raknaForlost", raknaForlost); 
         }
         else {
-          Grattis(datorVal, mittVal);
+          grattis(datorVal, mittVal);
           document.getElementById("utskriftId").innerHTML="Stenen förstör saxen.";
           document.getElementById("saxStenId").style.display="block";
           raknaVinst ++;
           localStorage.setItem("raknaVinst", raknaVinst);
         }
-        VinstForlustUtskrift(raknaForlost, raknaVinst);
+        vinstForlustUtskrift(raknaForlost, raknaVinst);
         break;
     
       case "sax":
         if (datorVal== "påse") {
-          Grattis(datorVal, mittVal);
+          grattis(datorVal, mittVal);
           document.getElementById("utskriftId").innerHTML="Saxen klipper sönder påsen.";
           document.getElementById("saxPaseId").style.display="block";
           raknaVinst ++;
           localStorage.setItem("raknaVinst", raknaVinst);
         }
         else {
-          Sorry(datorVal, mittVal);
+          sorry(datorVal, mittVal);
           document.getElementById("utskriftId").innerHTML="Saxen förstörs av stenen.";
           document.getElementById("saxStenId").style.display="block";
           raknaForlost ++;
           localStorage.setItem("raknaForlost", raknaForlost);
         }
-        VinstForlustUtskrift(raknaForlost, raknaVinst);
+        vinstForlustUtskrift(raknaForlost, raknaVinst);
         break;
 
       case "påse": 
           if (datorVal== "sax") {
-          Sorry(datorVal, mittVal);
+          sorry(datorVal, mittVal);
           document.getElementById("utskriftId").innerHTML="Påsen klipps sönder av saxen.";
           document.getElementById("saxPaseId").style.display="block";
           raknaForlost ++;
           localStorage.setItem("raknaForlost", raknaForlost);
         }
         else {
-          Grattis(datorVal, mittVal);
+          grattis(datorVal, mittVal);
           document.getElementById("utskriftId").innerHTML="Påsen omsluter stenen.";
           document.getElementById("paseStenId").style.display="block";
           raknaVinst ++;
           localStorage.setItem("raknaVinst", raknaVinst);
         }
-        VinstForlustUtskrift(raknaForlost, raknaVinst);
+        vinstForlustUtskrift(raknaForlost, raknaVinst);
         break;
       default: { //Sker om inget är valt.
           document.getElementById("utskriftHalvStorId").innerHTML="Du får välja något!";
@@ -79,23 +79,23 @@ function Spela() {  //Huvudfunktion. De funktioner som används i den är defini
     }
 }
 
-function Sorry(datorVal, mittVal) { //Utskrift. Datorval skickas med då det är definierat i funktionen Spela().
+function sorry(datorVal, mittVal) { //Utskrift. Datorval och mittVal skickas med då det är definierat i funktionen spela().
     document.getElementById("grattisSorryStorId").innerHTML="Sorry!"
     document.getElementById("utskriftHalvStorId").innerHTML="Datorn valde " + datorVal + " och du valde " + mittVal + ".";
 }
 
-function Grattis(datorVal, mittVal) { //Utskrift. Datorval skickas med då det är definierat i funktionen Spela().
+function grattis(datorVal, mittVal) { //Utskrift. Datorval och mittVal skickas med då det är definierat i funktionen spela().
     document.getElementById("grattisSorryStorId").innerHTML="Grattis!"
     document.getElementById("utskriftHalvStorId").innerHTML="Datorn valde " + datorVal + " och du valde " + mittVal + ".";
 }
 
-function VinstForlustUtskrift(raknaForlost, raknaVinst) { //Utskrifts funktion som även räknar ut om du leder eller ligger under. Variablerna skickas med då de definieras i funktionen Spela().
+function vinstForlustUtskrift(raknaForlost, raknaVinst) { //Utskrifts funktion som även räknar ut om du leder eller ligger under. Variablerna skickas med då de definieras i funktionen spela().
     if (raknaVinst==raknaForlost) document.getElementById("summeringId").innerHTML="Det är lika mellan er!<br><br>Antalet vinster: " + raknaVinst + "<br>Antalet förluster: " + raknaForlost;
     if (raknaVinst>raknaForlost) document.getElementById("summeringId").innerHTML="Du leder!<br><br>Antalet vinster: " + raknaVinst + "<br>Antalet förluster: " + raknaForlost;
     if (raknaVinst<raknaForlost) document.getElementById("summeringId").innerHTML="Du ligger under!<br><br>Antalet vinster: " + raknaVinst + "<br>Antalet förluster: " + raknaForlost;
 }
    
-function SlumpVal() { //Funktion som används till datorns val.
+function slumpVal() { //Funktion som används till datorns val.
     let slump =  Math.floor(Math.random()*3); //Slumpgeneratorn "Math.random" genererar tal från 0 upp till 1 (inte till och med 1). "floor" avrundar nedåt till närmaste heltal. Denna uträkning ger alltså ett tal med värdet 0, 1 eller 2.
     if (slump==0) {
       return "sten";
